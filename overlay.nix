8e93c0489f7344856a1a9271422f4493d0053804
Phosu Parsons <38191182+phossil@users.noreply.github.com>
2023-08-29 06:26:15 -0400

    overlays.default: `self: super` -> `final: prev`

--- overlay.nix
+++ overlay.nix
@@ -1,11 +1,11 @@
-self: super: {
-  plasma5Packages = super.plasma5Packages.overrideScope' (self: super: {
-    plasma5 = super.plasma5.overrideScope' (self: super: {
-      wrapland = self.callPackage ./wrapland { };
-      disman = self.callPackage ./disman { };
-      kdisplay = self.callPackage ./kdisplay { };
-      kwinft = self.callPackage ./kwinft { };
-      kwin = self.kwinft;
+final: prev: {
+  plasma5Packages = prev.plasma5Packages.overrideScope' (final: prev: {
+    plasma5 = prev.plasma5.overrideScope' (final: prev: {
+      wrapland = final.callPackage ./wrapland { };
+      disman = final.callPackage ./disman { };
+      kdisplay = final.callPackage ./kdisplay { };
+      kwinft = final.callPackage ./kwinft { };
+      kwin = final.kwinft;
     });
   });
 }
